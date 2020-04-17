c1c704aa388bb5fe9715d9e799e4190ecf08315b
abdulahiroble <abdulahi_2610@hotmail.com>
2020-04-17 23:40:31 +0200

    fixed anchor text

--- src/components/common/navigation/navigation.js
+++ src/components/common/navigation/navigation.js
@@ -15,7 +15,7 @@ import {
   ActionsContainer,
 } from "./style"
 
-const NAV_ITEMS = ["BOLIGVÆRDI"]
+const NAV_ITEMS = ["FORDELE"]
 
 export default class Navigation extends Component {
   state = {
@@ -48,7 +48,7 @@ export default class Navigation extends Component {
   }
 
   getNavAnchorLink = item => (
-    <AnchorLink href={`#${item.toLowerCase()}`} onClick={this.closeMobileMenu}>
+    <AnchorLink href={`#${item}`} onClick={this.closeMobileMenu}>
       {item}
     </AnchorLink>
   )
