fe735112055b7d5164b1cc5605365b848ea2a69d
abdulahiroble <abdulahi_2610@hotmail.com>
2020-04-19 00:16:46 +0200

    testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-advanced-sitemap`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
