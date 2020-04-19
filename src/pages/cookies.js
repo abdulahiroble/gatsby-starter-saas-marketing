2a8c5369bc3653bed010c66f39f1c7000cce956b
abdulahiroble <abdulahi_2610@hotmail.com>
2020-04-19 22:18:38 +0200

    google analytics implemented

--- src/pages/cookies.js
+++ src/pages/cookies.js
@@ -8,9 +8,34 @@ const NotFoundPage = () => (
     <SEO title="Om os" />
     <h1>Cookies</h1>
     <p>
-      Vi har kun et fomål. Vi vil gøre det simpelt for folk at vide hvor meget
-      deres bolig er værd
+      Hjemmesiden anvender "cookies". Cookies er en tekstfil, der gemmes på din
+      computer, mobiltelefon, tablet eller tilsvarende med det formål at
+      genkende den, huske indstillinger, udfører statistik og målrette annoncer.
+      Cookies kan ikke indeholde skadelig kode som f.eks virus. Du kan slette
+      eller blokerer cookies. Se hvordan her:{" "}
+      <a href="http://minecookies.org/cookiehandtering">
+        http://minecookies.org/cookiehandtering
+      </a>
     </p>
+
+    <h3>Nødvendige cookies</h3>
+
+    <p>
+      Nogle cookies kræves for at hjemmesiden kan fungere. Hjemmesiden fungerer
+      ikke korrekt uden disse cookies, og de er som standard aktiveret.
+    </p>
+
+    <h3>Google Analytics</h3>
+
+    <p>
+      Disse cookies giver os mulighed for at tælle besøgende og trafikkilder, så
+      vi kan måle og forbedre ydeevnen af vores hjemmeside. De hjælper os med at
+      finde ud af, hvilke sider der er mest og mindst populære samt hvordan de
+      besøgende bevæger sig på siden.
+    </p>
+    <a href="http://tools.google.com/dlpage/gaoptout">
+      Klik her for at fravælge cookies fra Google Analytics.
+    </a>
   </Layout>
 )
 
